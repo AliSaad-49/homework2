f052fabc56fe5423361b63e5fb1e1d0af73a994a
AliSaad-49 <aliedan94@mail.ru>
2021-03-01 21:56:12 +0300

    homework2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace geekbrains2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(" Ввидите число: ");
+
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(" Чётное Число");
+                    Console.ReadLine();
+                    break;
+
+                }
+
+                else
+                    Console.WriteLine("Ощибка!!, Вы ввели не чётное число");
+                Console.ReadLine();
+            }
+        }
+    }
+}
